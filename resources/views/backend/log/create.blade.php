34df179304563b267242ef42b814eeb2780201fd
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2019-07-28 23:52:46 +0800

    Added Log Repository.

--- resources/views/backend/log/create.blade.php
+++ resources/views/backend/log/create.blade.php
@@ -27,7 +27,8 @@
                                 </thead>
                                 <tbody>
                                 @foreach($activities as $activity)
-                                    <tr class="customer_activity" data-activity_id="{{ $activity->id }}"
+                                    <tr class="customer_activity"
+                                        data-activity_id="{{ $activity->id }}"
                                         data-status="{{  in_array($activity->id, $existingActivities) ?
                                             "subscribed"
                                             : "N/A" }}">
@@ -66,9 +67,11 @@
         $(document).on('click', 'tr.customer_activity', function (e) {
             const customerActivity = $(this),
                 coachContainer = $("#coach_container");
-            let activityRoute = "{{ route('admin.activity.show', ':activityId') }}",
+            let customerActivityRoute = "{{ route('admin.customer.activity.show', ['customer' => ':customerId', 'activity' => ':activityId']) }}",
                 html = null;
-            activityRoute = activityRoute.replace(':activityId', customerActivity.data('activity_id'));
+
+            customerActivityRoute = customerActivityRoute.replace(':activityId', customerActivity.data('activity_id'));
+            customerActivityRoute = customerActivityRoute.replace(':customerId', "{{ $customer->id }}");
 
             $(".customer_activity").each(function () {
                 if ($(this).hasClass('selected_activity')) {
@@ -80,16 +83,17 @@
 
             $.ajax({
                 type: "GET",
-                url: activityRoute,
+                url: customerActivityRoute,
                 dataType: "JSON"
-            }).done(function (activity) {
-                console.log(activity);
+            }).done(function (data) {
+                const storeLogRoute = "{{ route('admin.logs.store') }}";
+                console.log(data);
 
-                html = "<div class='card animated fadeInRight' id='activity-'" + activity.id + "'>";
+                html = "<div class='card animated fadeInRight' id='activity-'" + data.activity.id + "'>";
                 html += "<div class='card-body'>";
                 html += "<div class='row'>";
                 html += "<div class='col-12'>";
-                html += "<h4 class='card-title'>" + activity.name + " Information</h4>";
+                html += "<h4 class='card-title'>" + data.activity.name + " Information</h4>";
                 html += "</div> <!-- col-12 -->";
                 html += "</div> <!-- row -->";
 
@@ -97,69 +101,76 @@
 
                 html += "<div class='row'>";
                 html += "<div class='col-12'>";
-                html += "<form class='form-horizontal'>";
+                html += "<form class='form-horizontal' action='" + storeLogRoute + "' method='POST'>";
+                html += '{{ csrf_field() }}';
+                html += '<input type="hidden" value="' + data.activity.id + '" name="activity_id" id="activity_id" />';
+                html += '<input type="hidden" value="{{ $customer->id }}" name="customer_id" id="customer_id" />';
+                html += '<input type="hidden" value="' + data.membership_id + '" name="membership_id" id="membership_id" />';
                 html += "<div class='form-group row'>";
-                html += "<label id='coaches' class='col-2'>Coaches</label>";
+                html += "<label id='coaches' class='col-3'>Coaches</label>";
 
-                html += "<div class='col-10'>";
-                html += "<select class='form-control select2-single' name='coaches' id='coaches' value='" + activity.sessions + "'>";
+                html += "<div class='col-9'>";
+                html += "<select class='form-control select2-single' name='coach' id='coach'>";
                 html += "<option value=''></option>";
-                for (let i = 0; i < Object.keys(activity.activity_coaches).length; i++) {
-                    let coach = activity.activity_coaches[i];
-
-                    html += "<option value='" + coach.id + "'>" + coach.name + "</option>";
-                }
                 html += "</select>";
                 html += "</div>";
                 html += "</div> <!-- form-group -->";
 
                 if (customerActivity.data('status') == "N/A") {
                     html += "<div class='form-group row'>";
-                    html += "<label id='remaining_quota' class='col-2'>Remaining Quota</label>";
+                    html += "<label id='remaining_quota' class='col-3'>Remaining Quota</label>";
 
-                    html += "<div class='col-10'>" +
-                        "<input class='form-control' name='remaining_quota' id='remaining_quota' value='" + activity.quota + "' disabled> " +
+                    html += "<div class='col-9'>" +
+                        "<input class='form-control' name='remaining_quota' id='remaining_quota' value='" + data.activity.quota + "' disabled> " +
                         "</div>";
                     html += "</div> <!-- form-group -->";
                 }
 
                 if (customerActivity.data('status') != "N/A") {
                     html += "<div class='form-group row'>";
-                    html += "<label id='remaining_session' class='col-2'>Remaining Sessions</label>";
+                    html += "<label id='remaining_session' class='col-3'>Remaining Sessions</label>";
 
-                    html += "<div class='col-10'>" +
-                        "<input class='form-control' name='remaning_session' id='remaining_session' value='" + activity.sessions + "' disabled> " +
+                    html += "<div class='col-9'>" +
+                        "<input class='form-control' name='remaning_session' id='remaining_session' value='" + data.activity.sessions + "' disabled> " +
                         "</div>";
                     html += "</div> <!-- form-group -->";
                 } else {
-                    if (activity.quota != 0) {
+                    if (data.activity.quota != 0) {
                         html += "<div class='form-group row'>";
-                        html += "<label id='remaining_session' class='col-2'>Non-Member Rate</label>";
+                        html += "<label id='remaining_session' class='col-3'>Non-Member Rate</label>";
 
-                        html += "<div class='col-10'>" +
-                            "<input class='form-control' name='remaning_session' id='remaining_session' value='" + activity.non_member_rate + "' disabled> " +
+                        html += "<div class='col-9'>" +
+                            "<input class='form-control' name='remaning_session' id='remaining_session' value='" + data.activity.non_member_rate + "' disabled> " +
                             "</div>";
                         html += "</div> <!-- form-group -->";
                     } else {
-                        let shareIncome = parseFloat(activity.non_member_rate / 2);
+                        let shareIncome = parseFloat(data.activity.non_member_rate / 2);
 
                         html += "<div class='form-group row'>";
-                        html += "<label id='gym_income' class='col-2'>Gym Income</label>";
+                        html += "<label id='gym_income' class='col-3'>Gym Income</label>";
 
-                        html += "<div class='col-10'>" +
+                        html += "<div class='col-9'>" +
                             "<input class='form-control' name='gym_income' id='gym_income' value='" + shareIncome + "' disabled> " +
                             "</div>";
                         html += "</div> <!-- form-group -->";
 
                         html += "<div class='form-group row'>";
-                        html += "<label id='coach_income' class='col-2'>Coach Income</label>";
+                        html += "<label id='coach_income' class='col-3'>Coach Income</label>";
 
-                        html += "<div class='col-10'>" +
+                        html += "<div class='col-9'>" +
                             "<input class='form-control' name='coach_income' id='coach_income' value='" + shareIncome + "' disabled> " +
                             "</div>";
                         html += "</div> <!-- form-group -->";
                     }
                 }
+
+                html += "<div class='form-group row'>";
+                html += "<label id='coach_income' class='col-3'></label>";
+
+                html += "<div class='col-9'>" +
+                    "<button type='submit' id='confirmLog' class='btn btn-primary'>Confirm</button>" +
+                    "</div>";
+                html += "</div> <!-- form-group -->";
 
                 html += "</form>";
                 html += "</div> <!-- col-12 -->";
@@ -168,16 +179,19 @@
                 html += "</div> <!-- animated -->";
 
                 coachContainer.html(html);
-                loadSelect();
+
+                // loadSelect();
+
+                $('select').select2({
+                    placeholder: "Select a coach",
+                    theme: "bootstrap",
+                    data: data.activityCoaches,
+                    escapeMarkup: function(markup) {
+                        return markup;
+                    }
+                })
             });
         });
-
-        function loadSelect() {
-            $(document).find('select').select2({
-                placeholder: "Select a coach",
-                theme: "bootstrap"
-            });
-        }
     </script>
 
 @endpush
